f2cbc18cb093e2aade5e44379c1ad43cd0124918
ralucabaksai <62606434+ralucabaksai@users.noreply.github.com>
2020-08-06 11:26:59 +0300

    Updates

---
+++ precache-manifest.00cff229374be7dc363b0df6b556d337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb01ec56571cfe07b7300d4af773f54",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "f765295bb50b223acfbe",
+    "url": "/Tic-Tac-Toe-React/static/css/main.f4d7af8a.chunk.css"
+  },
+  {
+    "revision": "391a73c7ec99a234f33d",
+    "url": "/Tic-Tac-Toe-React/static/js/2.d16c8304.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-React/static/js/2.d16c8304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f765295bb50b223acfbe",
+    "url": "/Tic-Tac-Toe-React/static/js/main.2aeeae4d.chunk.js"
+  },
+  {
+    "revision": "bcd4b90c97de9968d60c",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime-main.6cb5b3eb.js"
+  }
+]);
